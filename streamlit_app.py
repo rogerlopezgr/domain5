f6cb2fd465bcb3cd4b52ef4f29b72581dfbf9d6d
rogerlopezgr <146998821+rogerlopezgr@users.noreply.github.com>
2023-10-04 22:46:09 +0200

    Update streamlit_app.py
    multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.header('Subtítulo')
 lista_de_fruta = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.dataframe(lista_de_fruta)
+
+streamlit.multiselect("Pick some fruits:",list(lista_de_fruta.Fruit)
